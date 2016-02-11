f4f66affb384a217438bd2b61691fd0f078fb566
Andrew Sage <andrew@asae.co.uk>
2016-02-11 22:37:55 +0000

    Rouge comma removed from generated geojson

--- export_photo_walker_as_geojson.sh
+++ export_photo_walker_as_geojson.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 sqlite3 PhotoWalker.sqlite <<!
 .headers off
-.mode column
 .output ambient.json
-
-select '{"type":"Feature","geometry":{"type":"Point","coordinates":[' || zlatitude || ',' || zlongitude ||']}, "properties":{"brightness":'|| zbrightness || ', "timestamp":"' || strftime('%Y-%m-%d %H:%M:%S', datetime(ZTIMESTAMP+978307200, 'unixepoch', 'localtime')) || '"}},' from ZAMBIENT where datetime(ZTIMESTAMP+978307200, 'unixepoch', 'localtime') > '2016-02-11 18:00:00' LIMIT 1000;
+select '{"type":"Feature","geometry":{"type":"Point","coordinates":[' || zlatitude || ',' || zlongitude ||']}, "properties":{"brightness":'|| zbrightness || ', "timestamp":"' || strftime('%Y-%m-%d %H:%M:%S', datetime(ZTIMESTAMP+978307200, 'unixepoch', 'localtime')) || '"}},' from ZAMBIENT where datetime(ZTIMESTAMP+978307200, 'unixepoch', 'localtime') > '2016-02-11 18:00:00' LIMIT 2;
 !
+sed -i '' '$ s/.$//' ambient.json
 echo '{ "type": "FeatureCollection", "features": [' > ambient.geojson
 cat ambient.json >> ambient.geojson
 echo "]}" >> ambient.geojson
