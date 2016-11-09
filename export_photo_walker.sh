adae288fe9459c2e31bd136b39e9959f87e35980
Andrew Sage <andrew@asae.co.uk>
2016-11-09 21:03:26 +0000

    Export processing script added that processes all points.
     Please enter the commit message for your changes. Lines starting

--- export_photo_walker.sh
+++ export_photo_walker.sh
@@ -3,5 +3,5 @@ sqlite3 PhotoWalker.sqlite <<!
 .headers on
 .mode csv
 .output ambient.csv
-select zbrightness as brightness, zlatitude as latitude, zlongitude as longitude,strftime('%Y-%m-%d %H:%M:%S', datetime(ZTIMESTAMP+978307200, 'unixepoch', 'localtime')) as timestamp from ZAMBIENT where datetime(ZTIMESTAMP+978307200, 'unixepoch', 'localtime') > '2016-02-11 18:00:00' LIMIT 1000;
+select zbrightness as brightness, zlatitude as latitude, zlongitude as longitude,strftime('%Y-%m-%d %H:%M:%S', datetime(ZTIMESTAMP+978307200, 'unixepoch', 'localtime')) as timestamp from ZAMBIENT where datetime(ZTIMESTAMP+978307200, 'unixepoch', 'localtime') > '2016-02-11 18:00:00';
 !
